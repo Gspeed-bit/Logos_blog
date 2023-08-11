3932582155b9951a5f4ad6805347517f6a432a86
Ayodeji Daniel Adeyeye <adeyeye17@gmail.com>
2023-08-11 17:53:54 +0200

    i use a dropdown to display the data instead of the modal i used before

--- pages/logos/[id].js
+++ pages/logos/[id].js
@@ -1,68 +1,90 @@
-// Import the required styles from the module
-import Styles from "../../styles/Home.module.css";
 
-// Define paths for dynamic routes using fetched data
-export const getStaticPaths = async () => {
-  // Fetch user data from an API
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
+//i have commented this because i choose to use a dropdown instead of a modal
 
-  // Create an array of paths for each user
-  const paths = data.map((eachList) => {
-    const { id } = eachList;
-    return {
-      params: { id: id.toString() },
-    };
-  });
 
-  // Return the paths along with the fallback setting
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// // Import the required styles from the module
+// import Styles from "../../styles/Home.module.css";
+// import { useState } from "react";
+// // Define paths for dynamic routes using fetched data
+// export const getStaticPaths = async () => {
+//   // Fetch user data from an API
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await response.json();
 
-// Fetch specific user data based on the dynamic route parameter
-export const getStaticProps = async ({ params }) => {
-  // Fetch the user data based on the id from the dynamic route
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params.id}`
-  );
-  const list = await response.json();
+//   // Create an array of paths for each user
+//   const paths = data.map((eachList) => {
+//     const { id } = eachList;
+//     return {
+//       params: { id: id.toString() },
+//     };
+//   });
 
-  // Return the fetched user data as props
-  return {
-    props: {
-      list,
-    },
-  };
-};
+//   // Return the paths along with the fallback setting
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-// Component to display user details
-const Details = ({ list }) => {
-  // Destructure the user details from the props
-  const { id, name, email, address } = list;
+// // Fetch specific user data based on the dynamic route parameter
+// export const getStaticProps = async ({ params }) => {
+//   // Fetch the user data based on the id from the dynamic route
+//   const response = await fetch(
+//     `https://jsonplaceholder.typicode.com/users/${params.id}`
+//   );
+//   const list = await response.json();
 
-  return (
-    <div className={Styles.container}>
-      <div className={Styles.detailsContainer}>
-        {/* Display user details */}
-        <p>
-          <span>Name:</span> {name}
-        </p>
-        <p>
-          <span>Email:</span> {email}
-        </p>
-        <p>
-          <span>City:</span> {address.city}
-        </p>
-        <p>
-          <span>Phone:</span> (+49){list.phone}
-        </p>
-      </div>
-    </div>
-  );
-};
+//   // Return the fetched user data as props
+//   return {
+//     props: {
+//       list,
+//     },
+//   };
+// };
 
-// Export the Details component as the default export
-export default Details;
+// // Component to display user details
+// const Details = ({ list }) => {
+//   // Destructure the user details from the props
+//    const { id, name, email, address } = list;
+
+//    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+//    const toggleDropdown = () => {
+//      setIsDropdownOpen(!isDropdownOpen);
+//    };
+
+//   return (
+//     <div className={Styles.container}>
+//       <div className={Styles.detailsContainer}>
+//         {/* Display user name and a button to toggle the dropdown */}
+//         <div className={Styles.userNameContainer}>
+//           <p>
+//             <span>Name:</span> {name}
+//           </p>
+//           <button onClick={toggleDropdown}>
+//             {isDropdownOpen ? "Close Details" : "Show Details"}
+//           </button>
+//         </div>
+        
+//         {/* Dropdown content */}
+//         {isDropdownOpen && (
+//           <div className={Styles.dropdownContent}>
+//             <p>
+//               <span>Email:</span> {email}
+//             </p>
+//             <p>
+//               <span>City:</span> {address.city}
+//             </p>
+//             <p>
+//               <span>Phone:</span> (+49) {list.phone}
+//             </p>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+
+// };
+
+// // Export the Details component as the default export
+// export default Details;
--- pages/logos/index.js
+++ pages/logos/index.js
@@ -1,8 +1,9 @@
-import Head from "next/head";
-import Styles from "../../styles/Home.module.css";
-import Link from "next/link";
+import React, { useState } from "react";
+import Head from "next/head"; // Import the Head component for setting document head properties
+import Styles from "../../styles/Home.module.css"; // Import your CSS styles
+import Link from "next/link"; // Import the Link component for navigation
 
-
+// Fetch the data from the API during build time using getStaticProps
 export const getStaticProps = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
@@ -12,29 +13,54 @@ export const getStaticProps = async () => {
   };
 };
 
+// Define the Contact component
 const Contact = ({ data }) => {
+  // State to keep track of the selected contact's ID
+  const [selectedContact, setSelectedContact] = useState(null);
+
+  // Function to handle contact click and set the selected contact's ID
+  const handleContactClick = (id) => {
+    // Toggle the selected contact if it's already open, otherwise set the new one
+    setSelectedContact((prevSelected) => (prevSelected === id ? null : id));
+  };
+
+  // JSX markup
   return (
     <>
+
       <Head>
         <title>Living_Logos | Logos </title>
         <link rel="icon" href="/vercel.png" />
       </Head>
+      {/* Container for the contact list */}
       <div className={Styles.logosContainer}>
         <h1> Minister's Contact </h1>
-           {data.map((list) => {
-              const { id, name } = list;
-              return (
-                <Link href={`./logos/${id}`} key={id}>
-                  <div className={Styles.contactList}>
-                    <h4>{name}</h4>
-                  </div>
-                </Link>
-              );
-            })}
-
-     </div>
-        </>
+        {/* Map through the data and display each contact */}
+        {data.map((contact) => {
+          const { id, name, email, phone, website } = contact;
+          return (
+            <div className={Styles.contactList} key={id}>
+              {/* Display the contact's name and attach a click handler */}
+              <h4
+                onClick={() => handleContactClick(id)}
+                className={Styles.contactList}
+              >
+                {name}
+              </h4>
+              {/* Display additional details if the contact is selected */}
+              {selectedContact === id && (
+                <div className={Styles.contactDetails}>
+                  <p>Email: {email}</p>
+                  <p>Phone: {phone}</p>
+                  <p>Website: {website}</p>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
-export default Contact;
+export default Contact; // Export the Contact component as the default export
