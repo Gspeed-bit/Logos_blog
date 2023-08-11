1ad9a892175c9ec630b4ac07b7a55b66a6816e19
Ayodeji Daniel Adeyeye <adeyeye17@gmail.com>
2023-08-11 18:02:51 +0200

    i fixed the build error

--- pages/logos/[id].js
+++ pages/logos/[id].js
@@ -1,10 +1,6 @@
+// Import the required styles from the module
+import Styles from "../../styles/Home.module.css";
 
-//i have commented this because i choose to use a dropdown instead of a modal
-
-
-// // Import the required styles from the module
-// import Styles from "../../styles/Home.module.css";
-// import { useState } from "react";
 // // Define paths for dynamic routes using fetched data
 // export const getStaticPaths = async () => {
 //   // Fetch user data from an API
@@ -42,49 +38,31 @@
 //   };
 // };
 
-// // Component to display user details
-// const Details = ({ list }) => {
-//   // Destructure the user details from the props
-//    const { id, name, email, address } = list;
+// Component to display user details
+const Details = ({ list }) => {
+  // Destructure the user details from the props
+  const { id, name, email, address } = list;
 
-//    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  return (
+    <div className={Styles.container}>
+      {/*<div className={Styles.detailsContainer}>
+        Display user details
+        <p>
+          <span>Name:</span> {name}
+        </p>
+        <p>
+          <span>Email:</span> {email}
+        </p>
+        <p>
+          <span>City:</span> {address.city}
+        </p>
+        <p>
+          <span>Phone:</span>(+49){list.phone}
+        </p>
+      </div> */}
+    </div>
+  );
+};
 
-//    const toggleDropdown = () => {
-//      setIsDropdownOpen(!isDropdownOpen);
-//    };
-
-//   return (
-//     <div className={Styles.container}>
-//       <div className={Styles.detailsContainer}>
-//         {/* Display user name and a button to toggle the dropdown */}
-//         <div className={Styles.userNameContainer}>
-//           <p>
-//             <span>Name:</span> {name}
-//           </p>
-//           <button onClick={toggleDropdown}>
-//             {isDropdownOpen ? "Close Details" : "Show Details"}
-//           </button>
-//         </div>
-        
-//         {/* Dropdown content */}
-//         {isDropdownOpen && (
-//           <div className={Styles.dropdownContent}>
-//             <p>
-//               <span>Email:</span> {email}
-//             </p>
-//             <p>
-//               <span>City:</span> {address.city}
-//             </p>
-//             <p>
-//               <span>Phone:</span> (+49) {list.phone}
-//             </p>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-
-// };
-
-// // Export the Details component as the default export
-// export default Details;
+// Export the Details component as the default export
+export default Details;
